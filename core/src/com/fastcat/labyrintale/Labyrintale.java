73692ac804e2e3318e65c35b7d55f28f964f86c5
FastCat <bafguit9929@gmail.com>
2022-12-17 23:54:20 +0900

    화면 전환 이미지 변경. 이 커밋 이후론 에셋 매니저 변환 끝날 때까지 FileHandler 사용 불가 (최종)

--- core/src/com/fastcat/labyrintale/Labyrintale.java
+++ core/src/com/fastcat/labyrintale/Labyrintale.java
@@ -173,8 +173,8 @@ public class Labyrintale extends Game {
 
     @Override
     public void create() {
-        //Gdx.graphics.setResizable(false);
-        //Gdx.graphics.setTitle("Wakest Dungeon - " + BuildInfo.BUILD_VERSION);
+        Gdx.graphics.setResizable(false);
+        Gdx.graphics.setTitle("Wakest Dungeon");
         if(Gdx.app.getType() == Application.ApplicationType.Desktop) {
             Pixmap pix = new Pixmap(Gdx.files.internal("img/ui/cursor_b.png"));
             pix.setFilter(Pixmap.Filter.BiLinear);
