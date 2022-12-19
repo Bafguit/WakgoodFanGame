944ec028f197343e147c0ee7927d4785f3774d57
FastCat <bafguit9929@gmail.com>
2022-12-19 11:06:17 +0900

    크레딧 ui 위치 수정, 릴리즈 오류 해결

--- core/src/com/fastcat/labyrintale/handlers/SettingHandler.java
+++ core/src/com/fastcat/labyrintale/handlers/SettingHandler.java
@@ -1,5 +1,6 @@
 package com.fastcat.labyrintale.handlers;
 
+import com.badlogic.gdx.Gdx;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import java.io.File;
@@ -69,8 +70,7 @@ public final class SettingHandler {
 
     public static void save() {
         try {
-            // TODO java.File 사용하는 부분 해결 필요
-            mapper.writeValue(settingFile, setting);
+            mapper.writeValue(Gdx.files.local("setting.json").file(), setting);
         } catch (IOException e) {
             e.printStackTrace();
         }
--- core/src/com/fastcat/labyrintale/screens/credit/CreditBackButton.java
+++ core/src/com/fastcat/labyrintale/screens/credit/CreditBackButton.java
@@ -22,7 +22,7 @@ public class CreditBackButton extends AbstractUI {
 
     public CreditBackButton() {
         super(FileHandler.getUi().get("BUTTON_S"));
-        setPosition(Gdx.graphics.getWidth() * 0.02f, 1145 * InputHandler.scale);
+        setPosition(Gdx.graphics.getWidth() * 0.02f, 1296 * InputHandler.scale);
         fontData = new FontHandler.FontData(MEDIUM, 53, false, false);
         text = "나가기";
     }
